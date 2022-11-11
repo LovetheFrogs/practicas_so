49ed6c6854eb4c0675e59d242e2fc6318ebfc116
LovetheFrogs <102818341+LovetheFrogs@users.noreply.github.com>
2022-11-11 16:02:18 +0100

    Fix: libreria.head method

--- Practica_1/libreria.c
+++ Practica_1/libreria.c
@@ -1,23 +1,48 @@
+#include "libreria.h"
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-int head(int n){
-    int i;
-    char *buffer = NULL;
-    size_t bufsize;
+#define BUFFERSIZE 1024
 
-    if (n == NULL){
-        n = 10;
+typedef struct node 
+{
+        struct node * prev;
+        char value[BUFFERSIZE];
+        struct node * next;
+} node_t;
+
+int head(int N) {
+    node_t * head = NULL;
+    head = (node_t *) malloc(sizeof(node_t));
+    head -> prev = NULL;
+    head -> next = NULL;
+
+    node_t * aux;
+    node_t * current = head;
+
+    while (fgets(current -> value, BUFFERSIZE, stdin) != NULL) {
+        aux = (node_t *) malloc(sizeof(node_t));
+        aux -> prev = current;
+        aux -> next = NULL;
+        current -> next = aux;
+        current = current -> next;
     }
-    
-    for (i = 0; i <= n; i++)
-    {
-        printf("%d\n", getline(&buffer,&bufsize,stdin));
+    free(aux);
+    current = head;
+    for (int i = 1; i <= N; i++) {
+        fputs(current -> value, stdout);
+        current = current -> next;
     }
-    return 0;
+
+    current = head;
+    while (current -> next != NULL) {
+        current = current -> next;
+        free(current -> prev);
+    }
 }
 
-int tail(int n) {
+int tail(int N) {
     char text[100];
     int i = 0;
     char *buffer = NULL;
@@ -30,14 +55,14 @@ int tail(int n) {
         text[i] == getline(&buffer, &bufsize, stdin);
     }
 
-    for (int j = i - 1; j <= i - n - 1; j--) {
-        printf("%d\n", text[j]);
+    for (int j = i - 1; j <= i - N - 1; j--) {
+        printf("%s\n", text[j]);
     }
 
     return 0;
 }
 
-int longlines(int n) {
+int longlines(int N) {
     char text[100];
     int i = 0;
     char *buffer = NULL;
@@ -60,16 +85,15 @@ static void sort(int lenght, char *ptr, int n){
     for (i = 0; i < lenght; i++) {
         for (j = i + 1; j < lenght; j++) {
             if (strlen(*(ptr + j)) < strlen(*(ptr + i))) {
-                t = strcp(*(ptr + i));
-                *(ptr + i) = strcp(*(ptr + j));
-                *(ptr + j) = strcp(t);
+                strcpy(t, *(ptr + i));
+                strcpy(*(ptr + i), *(ptr + j));
+                strcpy(*(ptr + j) , t);
             }
         }
     }
 
     for (i = 0; i < n; i++)
     {
-        printf("%d\n", *(ptr + i));
+        printf("%s\n", *(ptr + i));
     }
-    
 }
