484e254c5958de5356f12579ea542b28c6ddfab5
LovetheFrogs <102818341+LovetheFrogs@users.noreply.github.com>
2022-11-11 16:24:10 +0100

    Added comments for head method and improved code

--- Practica_1/libreria.c
+++ Practica_1/libreria.c
@@ -3,43 +3,75 @@
 #include <string.h>
 #include <stdlib.h>
 
+/* Definimos una constante BUFFERSIZE (tamaño máximo de una línea) */
 #define BUFFERSIZE 1024
 
+/* Definimos el nodo de una lista doblemente enlazada */
 typedef struct node 
 {
-        struct node * prev;
-        char value[BUFFERSIZE];
-        struct node * next;
+        struct node * prev;                 /* Puntero al nodo anterior */
+        char value[BUFFERSIZE];             /* Valor de esa posición */
+        struct node * next;                 /* Puntero al nodo siguiente */
 } node_t;
 
+/* Función head(N) */
 int head(int N) {
+    /* Creamos un nodo head como inicio de la lista, reservamos memoria para él y apuntamos prev y next a NULL */
     node_t * head = NULL;
     head = (node_t *) malloc(sizeof(node_t));
     head -> prev = NULL;
     head -> next = NULL;
 
+    /* Creamos un nodo auxiliar usado para añadir elementos */
     node_t * aux;
+
+    /* Creamos un nodo current y lo apuntamos a head (inicio de la lista) */
     node_t * current = head;
 
-    while (fgets(current -> value, BUFFERSIZE, stdin) != NULL) {
-        aux = (node_t *) malloc(sizeof(node_t));
-        aux -> prev = current;
-        aux -> next = NULL;
-        current -> next = aux;
-        current = current -> next;
-    }
-    free(aux);
-    current = head;
-    for (int i = 1; i <= N; i++) {
-        fputs(current -> value, stdout);
-        current = current -> next;
+    /* Iteramos tantas veces como líneas queramos leer */
+    for (int i = 0; i <= N; i++) {
+        /* Si la linea leida no es NULL, se crea un nodo aux, se enlaza a la lista y se mueve current al nuevo nodo */
+        if (fgets(current -> value, BUFFERSIZE, stdin) != NULL) {     /* La sentencia fgets(current -> value...) pone el valor del nodo apuntado por current a la línea leida */
+            aux = (node_t *) malloc(sizeof(node_t));
+            aux -> prev = current;
+            aux -> next = NULL;
+            current -> next = aux;
+            current = current -> next;
+        }
+        /* Si se ha llegado al final antes de que i > n, se sale del bucle */
+        else break;
     }
 
+    /* En caso de necesitar leer todo stdin, cambiar el FOR por este WHILE
+
+    while (fgets(current -> value, BUFFERSIZE, stdin) != NULL) {
+        if (i <= N) {
+            aux = (node_t *) malloc(sizeof(node_t));
+            aux -> prev = current;
+            aux -> next = NULL;
+            current -> next = aux;
+            current = current -> next;
+        }  
+    }
+    */
+    
+    /* Liberamos memoria y apuntamos current de nuevo a head para recorrer la lista de nuevo */
+    free(aux);
     current = head;
+
+    /* Iteramos en la lista hasta llegar al final */
     while (current -> next != NULL) {
+        /* Escribimos por pantalla el valor de current */
+        fputs(current -> value, stdout);
+
+        /* Pasamos al siguiente elemento */
         current = current -> next;
+
+        /* Liberamos el espacio ocupado por el elemento anterior */
         free(current -> prev);
     }
+
+    return 0;
 }
 
 int tail(int N) {
